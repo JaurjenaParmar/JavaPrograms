05c815205a9e12ba7feb0c27a1f3b0a4edf734bc
JaurjenaParmar <87418471+JaurjenaParmar@users.noreply.github.com>
2022-06-28 00:42:15 +0530

    Add files via upload
    This file contains programs related to Calendar class and date formatting using SimpleDateFormat Class

--- Main.java
+++ Main.java
@@ -1,32 +1,36 @@
-
+import java.util.*;
+import java.text.*;
 public class Main
 {
 	public static void main(String[] args) {
-		int n=5;
-		// Inverted right angled Triangle Pattern
-		for(int i=5;i>=1;i--){
-		    for(int j=1;j<=i;j++){
-		        System.out.print("*");
-		    }
-		    System.out.println();
-		}
+		//Creating a Calendar class instance based on current time
+		Calendar calendar = Calendar.getInstance();
+		Date date=new Date();
+		//converting date and time in appropriate formats
+		SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Date with Format(yyyy-MM-dd) : "+formatter.format(date));
 		
+		formatter = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Date with Format(dd/MM/yyyy) : "+formatter.format(date));
 		
-		
-		// Triangle Pattern
-		for(int i=1;i<=5;i++){
-		    for(int j=i;j<n;j++){
-		        System.out.print(" ");
-		    }
-		    for(int j=1;j<=(2*i-1);j++){
-		        if(j==1||i==n||j==(2*i-1)){
-		            System.out.print("*");
-		        }
-		        else{
-		            System.out.print(" ");
-		        }
-		    }
-		    System.out.println();
-		}
+		formatter = new SimpleDateFormat("dd-MMM-yyyy");
+		System.out.println("Date with Format(dd-MMM-yyyy) : "+formatter.format(date));
+
+        formatter =new SimpleDateFormat("E, MMM dd yyyy");
+        System.out.println("Date with Format(E, MMM dd yyyy) : "+formatter.format(date));
+        
+        System.out.println("\nCurrent date and Time is : "+calendar.getTime());
+        //Adding 1 month, 2 days and 4 years to current date and Time
+        calendar.add(Calendar.MONTH,1);
+        calendar.add(Calendar.DATE,2);
+        calendar.add(Calendar.YEAR, 4);
+        System.out.println("\n****After 1 month, 2 days and 4 years****");
+        System.out.println("Date and Time : "+calendar.getTime());
+        
+        System.out.println("\n****Before 1 month, 2 days and 4 years****");
+        calendar.add(Calendar.MONTH,-1);
+        calendar.add(Calendar.DATE,-2);
+        calendar.add(Calendar.YEAR,-4);
+        System.out.println("Date and Time : "+calendar.getTime());
 	}
 }
