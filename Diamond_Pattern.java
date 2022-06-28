7db9332da6f5cda2dc7e064922749dbe15e41c3e
JaurjenaParmar <87418471+JaurjenaParmar@users.noreply.github.com>
2022-06-28 20:37:19 +0530

    Add files via upload
    This file contains the Diamond Pattern.

---
+++ Diamond_Pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Diamond_Pattern{
+	public static void main(String args[]){
+		int n=5;
+		// this is to print the upper half of the diamond
+		for(int i=1;i<=n;i++){
+			for(int j=i;j<n;j++){
+				System.out.print(" ");}
+			for(int j=1;j<=(2*i-1);j++){
+				System.out.print("*");}
+			System.out.println(); // strats a new line
+		}
+		// this prints the lower half of the pattern
+		for(int i=n-1;i>=1;i--){
+			for(int j=i;j<n;j++){
+				System.out.print(" ");}
+			for(int j=1;j<=(2*i-1);j++){
+				System.out.print("*");
+			}
+			System.out.println(); // starts a new line 
+		}
+}
+}
