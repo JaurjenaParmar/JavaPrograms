b176edfdc3667584109383dac8ba6bde59945efd
JaurjenaParmar <87418471+JaurjenaParmar@users.noreply.github.com>
2022-06-28 20:27:44 +0530

    Update PasswordValidator.java
    This program checks if the particular password entered by the user is valid or not as per the given conditions.

--- PasswordValidator.java
+++ PasswordValidator.java
@@ -4,17 +4,17 @@ public class PasswordValidator{
 		boolean a=true,b=false,c=false;
 		int total_digits=0;
 		for(int i=0;i<str.length();i++){
-			if ((Character.isLetterOrDigit(str.charAt(i)) == false)) {
+			if ((Character.isLetterOrDigit(str.charAt(i)) == false)) {  // checks if the password conatins digits or letters only
            			a=false;
 			}
-			if(Character.isDigit(str.charAt(i))){
+			if(Character.isDigit(str.charAt(i))){                  // counts how may digits are there in a password entered
 				total_digits++;
 			}
 		}
 		if(total_digits>=2){
 			b=true;
 		}
-		if(str.length()==6){
+		if(str.length()==6){       // checks if the password is of length 6 or not
 			c=true;
 		}
 		boolean result=false;
@@ -26,7 +26,7 @@ public class PasswordValidator{
 	public static void main(String args[]){
 		Scanner s=new Scanner(System.in);
 		String str=s.next();
-		boolean validity=isValid(str);
+		boolean validity=isValid(str);            
 		System.out.println("Password is Valid : " + validity);
 	}
 }
